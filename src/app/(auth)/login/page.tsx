c740e72ad0ac1411de387110cf778e76c45f6baf
Yudhishthirr <officialyudhishthir@gmail.com>
2026-04-23 21:09:12 +0530

    enhance the ui looks by adding theme desgin

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/features/auth/components/login-form";
 import { requireUnauth } from "@/lib/auth-utils";
 
 const Page = async () => {
-    await requireUnauth();
+    // await requireUnauth();
 
     return <LoginForm />;
 };
