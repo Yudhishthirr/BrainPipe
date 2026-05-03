f74ed92a8fa3981bf9acef5bf736688ca124a6a9
Yudhishthirr <officialyudhishthir@gmail.com>
2026-05-03 14:02:36 +0530

    add the gemini node and alos setup creidentials for ai nodes

--- src/config/node-components.ts
+++ src/config/node-components.ts
@@ -1,4 +1,5 @@
 import { InitialNode } from "@/components/initial-node";
+import { GeminiNode } from "@/features/execution/components/gemini/node";
 import { HttpRequestNode } from "@/features/execution/components/http-request/node";
 import { GoogleFormTrigger } from "@/features/triggers/components/google-form-trigger/node";
 import { ManualTriggerNode } from "@/features/triggers/components/manual-trigger/node";
@@ -12,6 +13,7 @@ export const nodeComponents = {
     [NodeType.HTTP_REQUEST]: HttpRequestNode,
     [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
     [NodeType.GOOGLE_FORM_TRIGGER]: GoogleFormTrigger,
+    [NodeType.GEMINI]: GeminiNode,
 } as const satisfies NodeTypes;
 
 export type RegisteredNodeType = keyof typeof nodeComponents;
--- src/features/execution/lib/executor-registry.ts
+++ src/features/execution/lib/executor-registry.ts
@@ -2,14 +2,16 @@ import { NodeType } from "@/generated/prisma/enums";
 import { NodeExecutor } from "../types";
 import { manualTriggerExecutor } from "@/features/triggers/components/manual-trigger/executor";
 import { httpTriggerExecutor } from "../components/http-request/executor";
+import { geminiExecutor } from "@/features/execution/components/gemini/executor";
 import { googleFormTriggerExecutor } from "@/features/triggers/components/google-form-trigger/executor";
 
-export const excutorRegistry: Record<NodeType,NodeExecutor> = {
+export const excutorRegistry: Partial<Record<NodeType,NodeExecutor>>= {
 
   [NodeType.INITIAL]: manualTriggerExecutor,
   [NodeType.MANUAL_TRIGGER]: manualTriggerExecutor,
   [NodeType.HTTP_REQUEST]: httpTriggerExecutor,
   [NodeType.GOOGLE_FORM_TRIGGER]: googleFormTriggerExecutor, // Use the same executor for Google Form Trigger for now
+  [NodeType.GEMINI]: geminiExecutor, // Add Gemini executor to the registry
 
 }
 
