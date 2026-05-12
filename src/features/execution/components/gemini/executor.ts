415d86e1293ac8a9764d2ca5af0d6736a09abae3
Yudhishthirr <officialyudhishthir@gmail.com>
2026-05-12 10:25:03 +0530

    remove the api key

--- src/features/execution/components/gemini/executor.ts
+++ src/features/execution/components/gemini/executor.ts
@@ -80,8 +80,7 @@ export const geminiExecutor: NodeExecutor<GeminiData> = async ({
     });
   });
 
-  // console.log(`this is the gamini api key ${credential?.value}`)
-  // console.log(`This is the Gemini API key after decrypt: ${decrypt(credential?.value)}`);
+  
   if (!credential) {
     await publish(
       geminiChannel().status({
@@ -97,14 +96,8 @@ export const geminiExecutor: NodeExecutor<GeminiData> = async ({
     // apiKey: process.env.GEMINI_API_KEY
   });
 
-  // const ai = new GoogleGenAI({
-  //   apiKey: "AIzaSyCyztaN5ax977lVbR8c_GxhguIVkAMkIkQ"
-  // });
-
-  // const response = await ai.models.generateContent({
-  //   model: "gemini-3-flash-preview",
-  //   contents: "Explain how AI works in a few words",
-  // });
+  
+  
   try {
     const { steps } = await step.ai.wrap(
       "gemini-generate-text",
